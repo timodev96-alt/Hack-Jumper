86fbb25ceeac85611efc64ab9248c354d91eba17
Timothy <timo.dev96@gmail.com>
2026-08-24 15:58:16 +0300

    added constans file and orginze the project
    also Fix the render bug so the game can be truely responsive and working on any device!

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
+#main.py
 import pygame
 import sys 
 
-import renderer
+from renderer import calculate_render_rect
+import constans
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 700
-TARGET_RATIO = SCREEN_WIDTH/SCREEN_HEIGHT
 
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), pygame.RESIZABLE)
-canvas = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
+screen = pygame.display.set_mode((constans.SCREEN_WIDTH,constans.SCREEN_HEIGHT), pygame.RESIZABLE)
+canvas = pygame.Surface((constans.SCREEN_WIDTH,constans.SCREEN_HEIGHT))
 
 pygame.display.set_caption("Hack Jumper")
 
 clock = pygame.time.Clock()
 running = True
-render_rect = renderer.calculate_render_rect(SCREEN_WIDTH,SCREEN_HEIGHT)
+render_rect = calculate_render_rect(constans.SCREEN_WIDTH,constans.SCREEN_HEIGHT)
 
 while running:
     for event in pygame.event.get():
@@ -24,13 +23,13 @@ while running:
             running=False
 
         elif event.type == pygame.VIDEORESIZE: #When the User Change the window Size!
-            render_rect = renderer.calculate_render_rect(event.w,event.h)
+            render_rect = calculate_render_rect(event.w,event.h)
 
     canvas.fill((130,200,240))
     scaled_surface = pygame.transform.smoothscale(canvas,(render_rect.width,render_rect.height))
     screen.blit(scaled_surface,(render_rect.x, render_rect.y))
     
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(constans.FPS)
 pygame.quit()
 sys.exit()
--- renderer.py
+++ renderer.py
@@ -1,14 +1,16 @@
+#renderer.py
 import pygame
 
-import main
+import constans
+
 def calculate_render_rect(win_w,win_h):
     current_ratio = win_w / win_h
-    if current_ratio > main.TARGET_RATIO:
+    if current_ratio > constans.TARGET_RATIO:
         render_h = win_h
-        render_w = (win_w / main.TARGET_RATIO)
+        render_w = (win_w / constans.TARGET_RATIO)
     else:
         render_w = win_h
-        render_h = int(win_w/main.TARGET_RATIO)
+        render_h = int(win_w/constans.TARGET_RATIO)
     render_x = (win_w - render_w) // 2
     render_y = (win_h - render_h) // 2
     return pygame.Rect(render_x, render_y, render_w, render_h)
