97cedb125de6a01ece23e2b9a7815ca3ecedf617
Timothy <timo.dev96@gmail.com>
2026-08-31 19:06:59 +0300

    fix a bug, now the game is truly responsive to any window size!

--- renderer.py
+++ renderer.py
@@ -7,10 +7,10 @@ def calculate_render_rect(win_w,win_h):
     current_ratio = win_w / win_h
     if current_ratio > constans.TARGET_RATIO:
         render_h = win_h
-        render_w = (win_w / constans.TARGET_RATIO)
+        render_w = int(win_h * constans.TARGET_RATIO)
     else:
         render_w = win_h
-        render_h = int(win_w/constans.TARGET_RATIO)
+        render_h = int(win_w / constans.TARGET_RATIO)
     render_x = (win_w - render_w) // 2
     render_y = (win_h - render_h) // 2
     return pygame.Rect(render_x, render_y, render_w, render_h)
